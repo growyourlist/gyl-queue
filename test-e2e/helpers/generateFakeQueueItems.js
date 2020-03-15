5b31d82070403c5142c7eacb5b7e01438c1744ea
gyl-dev <gyl-dev@posteo.eu>
2020-03-15 18:35:39 +0100

    Fake html/text emails for testing

--- test-e2e/helpers/generateFakeQueueItems.js
+++ test-e2e/helpers/generateFakeQueueItems.js
@@ -17,4 +17,40 @@ const generateFakeQueueItems = (numberOfItems, opts = {}) => {
 	})
 }
 
-module.exports = generateFakeQueueItems
+const generateFakeBody = i => {
+	const remainder = i % 3;
+	switch (remainder) {
+		case 1: return {
+			text: 'Test text and html',
+			html: '<p><i>Test text and html</i></p>',
+		}
+		case 2: return {
+			text: 'Test text only',
+		}
+		default: return {
+			html: '<p><strong>Test html only</strong></p>',
+		}
+	}
+}
+
+const generateIndividualSendFakeQueueItems = (numberOfItems, opts = {}) => {
+	return Array(numberOfItems).fill(null).map((_, i) => {
+		const subscriberId = uuid()
+		return newQueueItem({
+			type: 'send email',
+			subscriber: {
+				subscriberId,
+				email: `me${i}@test.localhost`,
+			},
+			subscriberId,
+			tagReason: opts.tagReason || null,
+			subject: 'Test individual send',
+			body: generateFakeBody(i),
+		})
+	})
+}
+
+module.exports = {
+	generateFakeQueueItems,
+	generateIndividualSendFakeQueueItems,
+}
